64f785c22e46fbac3416bb346936065110e76411
Kyle <kylepcole@gmail.com>
2020-05-14 14:04:20 -0400

    event crud

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -4,7 +4,7 @@ class EventsController < ApplicationController
 		events = Event.all
 		render json: events.to_json(
 				except: [:updated_at, :created_at, :trip_id],
-				include: [users: { except: [:password_digest, :updated_at, :created_at, :age, :username] }]
+				# include: [users: { except: [:password_digest, :updated_at, :created_at, :age, :username] }]
 			)
 	end
 
@@ -14,66 +14,61 @@ class EventsController < ApplicationController
 		render json: event.to_json(
 				except: [:user_id, :updated_at, :created_at],
 
-				include: [
-					user_name: { only: :name},
-					trip: {
-						except: [:user_id, :updated_at, :created_at],
-						include: [organizer: { only: :name}]
-						}
-				]
+				# include: [
+				# 	user_name: { only: :name},
+				# 	trip: {
+				# 		except: [:user_id, :updated_at, :created_at],
+				# 		include: [organizer: { only: :name}]
+				# 		}
+				# ]
 			)
 	end
 
 	def update
-		updateEvent = Event.find(params[:id])
+		# updateEvent = Event.find(params[:id])
 
-		start_date = Event.parseDateTimeString(params[:start])
-		end_date = Event.parseDateTimeString(params[:end])
+		# start_date = Event.parseDateTimeString(params[:start])
+		# end_date = Event.parseDateTimeString(params[:end])
 
-		updateEvent.event_type = params[:event_type]
-		updateEvent.description = params[:description]
-		updateEvent.start = Time.new(start_date[0], start_date[1], start_date[2], start_date[3], start_date[4])
-		updateEvent.end = Time.new(end_date[0], end_date[1], end_date[2], end_date[3], end_date[4])
-		updateEvent.save
+		# updateEvent.event_type = params[:event_type]
+		# updateEvent.description = params[:description]
+		# updateEvent.start = Time.new(start_date[0], start_date[1], start_date[2], start_date[3], start_date[4])
+		# updateEvent.end = Time.new(end_date[0], end_date[1], end_date[2], end_date[3], end_date[4])
+		# updateEvent.save
 
-		render json: updateEvent.to_json(
-				except: [:user_id, :updated_at, :created_at],
+		# render json: updateEvent.to_json(
+		# 		except: [:user_id, :updated_at, :created_at],
 
-				include: [
-					user_name: { only: :name},
-					trip: {
-						except: [:user_id, :updated_at, :created_at],
-						include: [organizer: { only: :name}]
-						}
-				]
-			)
+		# 		include: [
+		# 			user_name: { only: :name},
+		# 			trip: {
+		# 				except: [:user_id, :updated_at, :created_at],
+		# 				include: [organizer: { only: :name}]
+		# 				}
+		# 		]
+		# 	)
 	end
 
 	def create
-
-		start_date = Event.parseDateTimeString(params[:start])
-		end_date = Event.parseDateTimeString(params[:end])
+		start_datetime = Event.parseDateTimeString(params[:start]).join(',')
+		end_datetime = Event.parseDateTimeString(params[:end]).join(',')
 		
 		newEvent = Event.create(
-					event_type: params[:event_type], 
-					description: params[:description],
-					start: Time.new(start_date[0], start_date[1], start_date[2], start_date[3], start_date[4]),
-					end: Time.new(end_date[0], end_date[1], end_date[2], end_date[3], end_date[4]),
-					trip_id: params[:trip_id],
-					user_id: params[:user_id]
-					)
-	
-		render json: newEvent.to_json(
-				except: [:user_id, :updated_at, :created_at],
-
-				include: [
-					user_name: { only: :name},
-					trip: {
-						except: [:user_id, :updated_at, :created_at],
-						include: [organizer: { only: :name}]
-						}
-				]
-			)
+			title: params[:title],
+			start: Time.new(start_datetime),
+			end: Time.new(end_datetime),
+			all_day: params[:all_day],
+			location: params[:location],
+			latitude: params[:latitude],
+			longitude: params[:longitude],
+			company_agency: params[:company_agency],
+			reservation_number: params[:reservation_number],
+			notes: params[:notes],
+			trip_id: params[:trip],
+			category_id: params[:category],
+			user_id: params[:user],
+		)
+		render json: newEvent
 	end
 
 	def destroy
